055422fb8c80d4a46e1e68d5065887c8f7771aaf
JoseCabrejoVillarCampus <cabre920903@hotmil.com>
2023-02-22 21:32:15 -0500

    fix[coffe]

---
+++ script.js
@@ -0,0 +1,22 @@
+let obtn = document.getElementById("obtn")
+let tbtn = document.getElementById("tbtn")
+let trbtn = document.getElementById("trbtn")
+let fbtn = document.getElementById("fbtn")
+let imgchange = document.getElementById("imgchange")
+
+obtn.onclick=function(){
+
+    imgchange.src="./img/NEVADO_AREQUIPE_300ML.png";
+}
+tbtn.onclick=function(){
+
+    imgchange.src="./img/COVA-000049414_21042307472201.png";
+}
+trbtn.onclick=function(){
+
+    imgchange.src="./img/COVA-000049415_21042307474900.png";
+}
+fbtn.onclick=function(){
+
+    imgchange.src="./img/Nariño-454-gr.png";
+}
